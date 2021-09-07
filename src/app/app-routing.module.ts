e7d47af75a507addb087bb0915cefa5f2885b4c3
riki89 <saikoudiallo59@gmail.com>
2021-09-07 00:54:35 -0500

    members

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminContributionComponent } from './admin-contribution/admin-contribution.component';
 import { ContributionsComponent } from './contributions/contributions.component';
 
 const routes: Routes = [
-  {path:'',component: ContributionsComponent}
+  {path:'',component: ContributionsComponent},
+  {path: 'finance', component: AdminContributionComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContributionsComponent } from './contributions/contributions.component';
+import { AdminContributionComponent } from './admin-contribution/admin-contribution.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContributionsComponent
+    ContributionsComponent,
+    AdminContributionComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
