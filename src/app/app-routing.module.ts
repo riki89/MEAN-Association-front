ef75a6b69c0ff6afbfedb3d06984825218f98375
thenewcreation <galatafir@yahoo.com>
2021-09-06 23:56:36 -0500

    contributions page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContributionsComponent } from './contributions/contributions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: ContributionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
